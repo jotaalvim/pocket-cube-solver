113a5d6308693fdace9b135fcc5c9a5428c075fe
jotaalvim <joaoalvim179@proton.me>
2026-07-28 00:10:35 +0100

    rotation and orientationM; valid moves

---
+++ Cube.hs
@@ -0,0 +1,111 @@
+module Cube where
+
+
+import Utils
+
+--           ( X , Y  , Z  )
+type Point = (Int, Int, Int)
+
+type Orientation = Int
+
+data Piece = Red    [Point] Orientation 
+           | Orange [Point] Orientation 
+           | Blue   [Point] Orientation 
+           | Edge   [Point] Orientation
+           | Corner [Point] Orientation
+            -- | Center [Point] Orientation maybe redundant?
+    deriving (Eq, Show)
+
+data Move = U | D | B | F | L | R
+    deriving (Eq, Show)
+
+
+type Cube =  [Piece]
+
+solvedCube :: Cube
+solvedCube = [-- x y z
+        Corner [(2,2,2)] 0,
+        Edge   [(2,1,2)] 0,
+        Edge   [(1,2,2)] 0,
+        Edge   [(2,2,1)] 0,
+
+        Red    [(1,0,2),(2,0,1),(2,0,2)] 0,
+        Red    [(0,1,2),(0,2,2),(0,2,1)] 0,
+        Red    [(1,2,0),(2,2,0),(2,1,0)] 0,
+
+        Orange [(0,0,1),(0,0,2)] 0,
+        Orange [(0,1,0),(0,2,0)] 0,
+        Orange [(1,0,0),(2,0,0)] 0,
+
+        Blue [(0,0,0)] 0 
+       ]
+
+
+move :: Cube -> Move -> Cube
+move cube m = map turn cube
+  where turn p | any (affects m) (pointsOf p) = rotate p m
+               | otherwise                    = p
+
+possibleMoves :: Cube -> [Move]
+possibleMoves cube = filter (valid . move cube) [U,D,R,L,F,B]
+
+valid :: Cube -> Bool 
+valid = distinct . concatMap pointsOf 
+    
+
+affects :: Move -> Point -> Bool
+affects U (_,_,z) = z == 2
+affects D (_,_,z) = z == 0
+affects R (x,_,_) = x == 2
+affects L (x,_,_) = x == 0
+affects F (_,y,_) = y == 2
+affects B (_,y,_) = y == 0
+
+pointsOf :: Piece -> [Point]
+pointsOf (Corner l _) = l
+pointsOf (Edge   l _) = l
+pointsOf (Red    l _) = l
+pointsOf (Orange l _) = l
+pointsOf (Blue   l _) = l
+
+rotate :: Piece -> Move -> Piece
+rotate p@(Corner l _) m = Corner (rotateCords m l) (orient p m)
+rotate p@(Edge   l _) m = Edge   (rotateCords m l) (orient p m)
+rotate   (Red    l o) m = Red    (rotateCords m l) o
+rotate   (Orange l o) m = Orange (rotateCords m l) o
+rotate   (Blue   l o) m = Blue   (rotateCords m l) o
+
+axisOf :: Move -> Int
+axisOf U = 0
+axisOf D = 0
+axisOf L = 1
+axisOf R = 1
+axisOf F = 2
+axisOf B = 2
+
+orient :: Piece -> Move -> Orientation
+orient (Corner _ o) m
+    | axisOf m == o = o
+    | otherwise     = 3 - o - axisOf m 
+orient (Edge _ o) m
+    | axisOf m == 0 = o 
+    | otherwise     = 1 - o
+
+rotateCords :: Num c => Move -> [(c, c, c)] -> [(c, c, c)]
+rotateCords m = map moveBack . map (rotateCord m) . map moveCenter
+
+moveCenter, moveBack :: (Num a, Num b, Num c) => (a, b, c) -> (a, b, c)
+moveCenter (x,y,z) = (x-1,y-1,z-1)
+moveBack   (x,y,z) = (x+1,y+1,z+1)
+
+rotateCord :: Num a => Move -> (a, a, a) -> (a, a, a)
+rotateCord U (x,y,z) = (-y, x, z)
+rotateCord D (x,y,z) = ( y,-x, z)
+rotateCord R (x,y,z) = ( x,-z, y)
+rotateCord L (x,y,z) = ( x, z,-y)
+rotateCord F (x,y,z) = ( z, y,-x)
+rotateCord B (x,y,z) = (-z, y, x)
+
+
+main :: IO ()
+main = print (move solvedCube U)
